7098d3be88860fc5601e5458d6e4ad78ef37a3f7
Will Fong <willfong@pm.me>
2021-01-22 23:25:10 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+    <div
+      class="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl"
+    >
+      <div>
+        <h2
+          class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl"
+        >
+          000.farm
+        </h2>
+      </div>
+      <div class="mt-6 pt-10 grid gap-16 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
+        <div>
+          <p class="text-sm text-gray-500">
+            <time datetime="2020-03-16">Mar 16, 2020</time>
+          </p>
+          <a href="#" class="mt-2 block">
+            <p class="text-xl font-semibold text-gray-900">
+              Boost your conversion rate
+            </p>
+            <p class="mt-3 text-base text-gray-500">
+              Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam
+              vitae illo. Non aliquid explicabo necessitatibus unde. Sed
+              exercitationem placeat consectetur nulla deserunt vel. Iusto
+              corrupti dicta.
+            </p>
+          </a>
+          <div class="mt-3">
+            <a
+              href="#"
+              class="text-base font-semibold text-indigo-600 hover:text-indigo-500"
+            >
+              Read full story
+            </a>
+          </div>
+        </div>
+
+        <div>
+          <p class="text-sm text-gray-500">
+            <time datetime="2020-03-10">Mar 10, 2020</time>
+          </p>
+          <a href="#" class="mt-2 block">
+            <p class="text-xl font-semibold text-gray-900">
+              How to use search engine optimization to drive sales
+            </p>
+            <p class="mt-3 text-base text-gray-500">
+              Optio cum necessitatibus dolor voluptatum provident commodi et.
+              Qui aperiam fugiat nemo cumque.
+            </p>
+          </a>
+          <div class="mt-3">
+            <a
+              href="#"
+              class="text-base font-semibold text-indigo-600 hover:text-indigo-500"
+            >
+              Read full story
+            </a>
+          </div>
+        </div>
+
+        <div>
+          <p class="text-sm text-gray-500">
+            <time datetime="2020-02-12">Feb 12, 2020</time>
+          </p>
+          <a href="#" class="mt-2 block">
+            <p class="text-xl font-semibold text-gray-900">
+              Improve your customer experience
+            </p>
+            <p class="mt-3 text-base text-gray-500">
+              Cupiditate maiores ullam eveniet adipisci in doloribus nulla
+              minus. Voluptas iusto libero adipisci rem et corporis.
+            </p>
+          </a>
+          <div class="mt-3">
+            <a
+              href="#"
+              class="text-base font-semibold text-indigo-600 hover:text-indigo-500"
+            >
+              Read full story
+            </a>
+          </div>
+        </div>
+
+        <div>
+          <p class="text-sm text-gray-500">
+            <time datetime="2020-01-29">Jan 29, 2020</time>
+          </p>
+          <a href="#" class="mt-2 block">
+            <p class="text-xl font-semibold text-gray-900">
+              Writing effective landing page copy
+            </p>
+            <p class="mt-3 text-base text-gray-500">
+              Ipsum voluptates quia doloremque culpa qui eius. Id qui id officia
+              molestias quaerat deleniti. Qui facere numquam autem libero quae
+              cupiditate asperiores vitae cupiditate. Cumque id deleniti
+              explicabo.
+            </p>
+          </a>
+          <div class="mt-3">
+            <a
+              href="#"
+              class="text-base font-semibold text-indigo-600 hover:text-indigo-500"
+            >
+              Read full story
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
